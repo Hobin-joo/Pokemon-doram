bc9e087cb2ccf7d645de40bbb284454c89590de1
Hobin-joo <wnghqls12@naver.com>
2024-11-14 11:29:17 +0900

    리덕스완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Router } from "./shared/Router";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import Store from "./redux/store";
 import { ToastContainer } from "react-toastify";
-
 
 const App = () => {
   return (
     <>
-      <Provider store={store}>
+      <Provider Store={Store}>
         <Router />
         <ToastContainer />
       </Provider>
